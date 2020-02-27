e0ae615f40f42b64463f2a270e912b1699b1734a
Koshova Natalia <nata.nech@gmail.com>
2020-02-27 11:18:16 +0200

    vue

---
+++ lesson 25/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button @click='showModal' :visible="isModalVisible" >Oткрыть модальное окно</button>
+    <Modal v-show="isModalVisible"  @close="closeModal">
+      <template v-slot:header>
+          <h1>Header</h1>
+      </template>
+      <template v-slot:main>
+          <h1>Main</h1>
+      </template>
+      <template v-slot:footer>
+          <h1>Footer</h1>
+      </template> 
+    </Modal>
+    
+  </div>
+</template>
+
+<script>
+import Modal from './Modal'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      isModalVisible: false,
+    }
+  },
+  components: {
+    Modal: Modal
+  },
+  methods: {
+    closeModal(noVisible) {
+      this.isModalVisible = noVisible;
+    },
+   showModal() {
+     this.isModalVisible = true;
+   }
+  }
+}
+</script>
+
+<style  lang="scss">
+</style>
---
+++ lesson 25/Modal.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="modal-backdrop">
+    <div class="modal">
+      <button class="btn-close" @click="close">
+        x
+      </button>
+      <header class="modal-wrapper">
+        <slot name="header"></slot>
+      </header>
+      <main class="modal-wrapper">
+        <slot name="main"></slot>
+      </main>
+      <footer class="modal-wrapper">
+        <slot name="footer"></slot>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+   export default {
+     name:"Modal",
+     props:{
+         visible: Boolean,
+     },
+     data () {
+         return {
+         }
+     },
+     methods: {
+      close() {
+         this.$emit('close',this.visible);
+      }
+     }
+   }
+</script>
+
+<style lang='scss'>
+  .modal {
+    width: 400px;
+    height: 500px;
+    background: #FFFFFF;
+    position: relative;
+    left: 50%;
+    transform: translate(-50%);
+  }
+  .btn-close {
+    font-size: 20px;
+    padding: 10px;
+    cursor: pointer;
+    background: red;
+    position: fixed;
+    right: 0;
+    }
+  .modal-wrapper {
+      padding: 30px;
+  }
+</style>
